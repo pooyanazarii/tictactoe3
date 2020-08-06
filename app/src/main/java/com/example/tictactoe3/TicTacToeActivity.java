0ff4f2d835f0768d089fd098f278a88156538297
Pooya Nazari <npooya12@gmail.com>
2020-08-06 14:15:13 +0430

    add tag and animation && player var

--- app/src/main/java/com/example/tictactoe3/TicTacToeActivity.java
+++ app/src/main/java/com/example/tictactoe3/TicTacToeActivity.java
@@ -6,16 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class TicTacToeActivity extends AppCompatActivity {
+    public static final int KNIFE = 0;
+    public static final int SHIELD = 1;
+    public static final int NOT_PLAYED = 2;
+
+    int [] status = {NOT_PLAYED, NOT_PLAYED, NOT_PLAYED,
+                     NOT_PLAYED, NOT_PLAYED, NOT_PLAYED,
+                     NOT_PLAYED, NOT_PLAYED, NOT_PLAYED
+    };
+
+    int activePlayer = KNIFE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_ground);
+        Random randomPlayer = new Random();
+        activePlayer = (randomPlayer.nextInt(1));
     }
     public void dropIn(View view)
     {
         ImageView img = (ImageView) view;
+        img.setTranslationY(-2000f);
         img.setImageResource(R.drawable.knife);
+        //img.animate().translationY(0f).rotationBy(360*10).setDuration(500);
+        img.animate().translationY(0f).setDuration(150);
     }
 }
